f9b17fe92f26f4e47b4dbc3aa11828ca58ccc127
niannianwang <niw002@ucsd.edu>
2025-08-05 21:29:27 -0700

    astronauts fixed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,39 +7,34 @@ const astronautNames = [
   "Sarah Tran" , "Charlie Wu", "Sabeel Mansuri", "Joel Bisarra", "Joona Kim",
   "Jim Villani", "Lisa Takai", "Tara Len", "Thomas Rexin", "Katherine Vu",
   "Om Rabadia ", "Tyler Meyers", "Henry Lin", "Alexander Parrish" , "Brandon Nghiem", 
-  "Justin Hernandez", "Ethan Jorkins", "Max Zamorano"
+  "Justin Hernandez", "Ethan Jenkins", "Max Zamorano"
 ];
 
 const Footer = () => {
   return (
     <div className="footer">
-      <div className="footer-row">
-
+      <div className="footer-upper-row">
+        <p className="footer-blurb">
+          Launch our outreach program at your school today!
+        </p>
+        <nav>
+          <ul> 
+            <li><Link to="/contact">Contact Us</Link></li>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About Us</Link></li>
+            <li><Link to="/services">Services</Link></li>
+          </ul>
+        </nav> 
+      </div>
+      <div className="footer-bottom-row">
         <div className="rocket-container-left">
           <img src={Rocket} alt="rocket" className="rocket" />
         </div>
-
-
-        <div className="footer-right">
-          <div className="top">
-              <div className="footer-content">
-                <p className="footer-blurb">
-                  Launch our outreach program at your school today!
-                </p>
-                <nav>
-                  <ul> 
-                    <li><Link to="/contact">Contact Us</Link></li>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/about">About Us</Link></li>
-                    <li><Link to="/services">Services</Link></li>
-                  </ul>
-                </nav> 
-              </div>   
-          </div>
-          <div className = "bottom">
+        <div className="astronauts-block">
+          <div className="astronauts-scroll-wrapper">
             <div className="astronauts">
-            {astronautNames.map((name, i) => (
-              <div key={i} className="astronaut-wrapper">
+              {astronautNames.map((name, i) => (
+                <div key={i} className="astronaut-wrapper">
                   <img
                     src={Astronaut}
                     alt={`astronaut-${i}`}
