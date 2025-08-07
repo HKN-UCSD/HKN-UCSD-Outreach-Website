4b067cbac6e6178046a79fc94fc103db4620f129
jyeh2 <yehjames526@gmail.com>
2025-08-06 23:43:23 -0700

    Fixed Gallery Stacking

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,19 +1,17 @@
 import React from 'react';
 import './styles/Gallery.css'; // Assuming you have a CSS file for styling
-const Gallery = ({ rows }) => {
+
+const Gallery = ({ images }) => {
   return (
     <div className="gallery">
-      {rows.map((row, idx) => (
-        <div key={idx} className="gallery-row">
-          {row.map((image, imageIdx) => (
-            <div key={imageIdx} className="gallery-item">
-              <img src={image} alt={`gallery-item-${idx}-${imageIdx}`} className="gallery-image" />
-            </div>
-          ))}
+      {images.map((image, idx) => (
+        <div key={idx} className="gallery-item">
+          <img src={image} alt={`gallery-item-${idx}`} className="gallery-image" />
         </div>
       ))}
     </div>
   );
 };
+
 
 export default Gallery;
