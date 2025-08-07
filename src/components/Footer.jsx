7bafd07522245ff98f84df9601002fa3170bd58f
jyeh2 <yehjames526@gmail.com>
2025-08-06 18:03:21 -0700

    Reworked Footer, very very very modular now

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,54 +1,71 @@
-import Astronaut from "../assets/Astronaut.png"; 
+
+import './styles/Footer.css'
+import MatchedImageTextRow from "./MatchedImageTextRow";
+import { Link, useLocation } from "react-router-dom";
 import Rocket from "../assets/Rocket.png";
-import { Link } from "react-router-dom";
-import "./styles/Footer.css"; 
 
-const astronautNames = [
-  "Sarah Tran" , "Charlie Wu", "Sabeel Mansuri", "Joel Bisarra", "Joona Kim",
-  "Jim Villani", "Lisa Takai", "Tara Len", "Thomas Rexin", "Katherine Vu",
-  "Om Rabadia ", "Tyler Meyers", "Henry Lin", "Alexander Parrish" , "Brandon Nghiem", 
-  "Justin Hernandez", "Ethan Jenkins", "Max Zamorano"
-];
 
-const Footer = () => {
+
+
+export default function App() {
+  const location = useLocation();
+  const currentPath = location.pathname;
   return (
-    <div className="footer">
-      <div className="footer-upper-row">
-        <p className="footer-blurb">
-          Launch our outreach program at your school today!
-        </p>
-        <nav>
-          <ul> 
-            <li><Link to="/contact">Contact Us</Link></li>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/about">About Us</Link></li>
-            <li><Link to="/services">Services</Link></li>
-          </ul>
-        </nav> 
-      </div>
-      <div className="footer-bottom-row">
-        <div className="rocket-container-left">
-          <img src={Rocket} alt="rocket" className="rocket" />
-        </div>
-        <div className="astronauts-block">
-          <div className="astronauts-scroll-wrapper">
-            <div className="astronauts">
-              {astronautNames.map((name, i) => (
-                <div key={i} className="astronaut-wrapper">
-                  <img
-                    src={Astronaut}
-                    alt={`astronaut-${i}`}
-                    className="astronaut"
-                  />
-                  <span className="tooltip">{name}</span>
-                </div>
-              ))}
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
+    <footer className="footer">
+      <div className="containers">
+      <ul className="social-icon">
+        <li className="social-icon__item"><a className="social-icon__link" href="#">
+            <ion-icon name="logo-facebook"></ion-icon>
+          </a></li>
 
-export default Footer;
+        <li className="social-icon__item"><a className="social-icon__link" href="#">
+            <ion-icon name="logo-twitter"></ion-icon>
+          </a></li>
+
+        <li className="social-icon__item"><a className="social-icon__link" href="#">
+            <ion-icon name="logo-linkedin"></ion-icon>
+          </a></li>
+          
+        <li className="social-icon__item"><a className="social-icon__link" href="#">
+            <ion-icon name="logo-instagram"></ion-icon>
+          </a></li>
+      </ul>
+      
+      {/* Not Possible to match image height to text container height, so I created a seperate component called MatchImageTextRow */}
+      
+      <div className="footer">
+      <MatchedImageTextRow
+        imgSrc={Rocket}
+        alt="Rocket"
+        title="Launch our outreach program at your school today!"
+        body="We bring HKN to students across the country through engaging mentorship."
+      />
+      </div>
+
+      {/* Conditional Menu Links */}
+        <ul className="menu">
+          {currentPath !== "/about" && (
+            <li className="menu__item">
+              <Link className="menu__link" to="/about">About</Link>
+            </li>
+          )}
+          {currentPath !== "/services" && (
+            <li className="menu__item">
+              <Link className="menu__link" to="/services">Services</Link>
+            </li>
+          )}
+          {currentPath !== "/contact" && (
+            <li className="menu__item">
+              <Link className="menu__link" to="/contact">Contact</Link>
+            </li>
+          )}
+          {currentPath !== "/" && (
+            <li className="menu__item">
+              <Link className="menu__link" to="/">Home</Link>
+            </li>
+          )}
+        </ul>
+      </div>
+    </footer>
+  );
+}
