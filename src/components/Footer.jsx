c5e140b4d7e127960c63943e20c93d97df1f9ce1
jyeh2 <yehjames526@gmail.com>
2025-08-06 22:13:28 -0700

    footer complete

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,13 @@ import './styles/Footer.css'
 import MatchedImageTextRow from "./MatchedImageTextRow";
 import { Link, useLocation } from "react-router-dom";
 import Rocket from "../assets/Rocket.png";
-
+import Astronaut from "../assets/Astronaut.png"
+const astronautNames = [
+  "Sarah Tran" , "Charlie Wu", "Sabeel Mansuri", "Joel Bisarra", "Joona Kim",
+  "Jim Villani", "Lisa Takai", "Tara Len", "Thomas Rexin", "Katherine Vu",
+  "Om Rabadia ", "Tyler Meyers", "Henry Lin", "Alexander Parrish" , "Brandon Nghiem", 
+  "Justin Hernandez", "Ethan Jenkins", "Max Zamorano"
+];
 
 
 
@@ -13,58 +19,35 @@ export default function App() {
   return (
     <footer className="footer">
       <div className="containers">
-      <ul className="social-icon">
-        <li className="social-icon__item"><a className="social-icon__link" href="#">
-            <ion-icon name="logo-facebook"></ion-icon>
-          </a></li>
-
-        <li className="social-icon__item"><a className="social-icon__link" href="#">
-            <ion-icon name="logo-twitter"></ion-icon>
-          </a></li>
-
-        <li className="social-icon__item"><a className="social-icon__link" href="#">
-            <ion-icon name="logo-linkedin"></ion-icon>
-          </a></li>
-          
-        <li className="social-icon__item"><a className="social-icon__link" href="#">
-            <ion-icon name="logo-instagram"></ion-icon>
-          </a></li>
-      </ul>
       
       {/* Not Possible to match image height to text container height, so I created a seperate component called MatchImageTextRow */}
       
-      <div className="footer">
-      <MatchedImageTextRow
-        imgSrc={Rocket}
-        alt="Rocket"
-        title="Launch our outreach program at your school today!"
-        body="We bring HKN to students across the country through engaging mentorship."
-      />
-      </div>
+        <div className="footer">
+        <MatchedImageTextRow
+          imgSrc={Rocket}
+          alt="Rocket"
+          title="Launch our outreach program at your school today!"
+          body="We bring STEM to underrepresented schools across San Diego."
+        />
+        </div>
 
       {/* Conditional Menu Links */}
-        <ul className="menu">
-          {currentPath !== "/about" && (
-            <li className="menu__item">
-              <Link className="menu__link" to="/about">About</Link>
-            </li>
-          )}
-          {currentPath !== "/services" && (
-            <li className="menu__item">
-              <Link className="menu__link" to="/services">Services</Link>
-            </li>
-          )}
-          {currentPath !== "/contact" && (
-            <li className="menu__item">
-              <Link className="menu__link" to="/contact">Contact</Link>
-            </li>
-          )}
-          {currentPath !== "/" && (
-            <li className="menu__item">
-              <Link className="menu__link" to="/">Home</Link>
-            </li>
-          )}
-        </ul>
+        <div className="astronauts-block">
+          <div className="astronauts-scroll-wrapper">
+            <div className="astronauts">
+              {astronautNames.map((name, i) => (
+                <div key={i} className="astronaut-wrapper">
+                  <img
+                    src={Astronaut}
+                    alt={`astronaut-${i}`}
+                    className="astronaut"
+                  />
+                  <span className="tooltip">{name}</span>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
       </div>
     </footer>
   );
