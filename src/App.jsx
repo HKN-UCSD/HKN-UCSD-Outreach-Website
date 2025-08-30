98c32e50aa4c07efb846e95317ea4a7ad8cc57ef
jyeh2 <yehjames526@gmail.com>
2025-08-30 13:43:23 +0700

    deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,7 @@
+// App.jsx
 import React from "react";
-import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HKNOutreachPage from "./Home"
-import About from "./AboutUs";
-import Services from './Services';
+import HKNOutreachPage from "./Home";
 
-function App(){
-return(
-   <Router>
-    <Routes>
-      <Route path="/" element={<HKNOutreachPage />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/services" element={<Services />} />
-      {/* <Route path="/contact" element={<Contact />} />
-       */}
-    </Routes>
-  </Router>
-
-);
+export default function App() {
+  return <HKNOutreachPage />;
 }
-
-export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { Route } from 'react-router-dom'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import App from './App'
+import About from './AboutUs'
+import Services from './Services'
 import './index.css'
-import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
 )
