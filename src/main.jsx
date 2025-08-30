0fa6dcf0ca510192a54a430ba6961b7ccc178196
jyeh2 <yehjames526@gmail.com>
2025-08-30 13:44:46 +0700

    deployment

--- src/main.jsx
+++ src/main.jsx
@@ -11,8 +11,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/services" element={<Services />} />
+        <Route path="/HKN-UCSD-Outreach-Website/about" element={<About />} />
+        <Route path="/HKN-UCSD-Outreach-Website/services" element={<Services />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
