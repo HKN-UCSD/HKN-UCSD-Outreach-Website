97cc852366dea9bdfe05d4207b0cdc186b96cef6
jyeh2 <yehjames526@gmail.com>
2025-08-30 14:12:16 +0700

    finished

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import App from './App'
 import About from './AboutUs'
 import Services from './Services'
@@ -8,12 +8,12 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <Router>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/about" element={<About />} />
         <Route path="/services" element={<Services />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   </React.StrictMode>
 )
